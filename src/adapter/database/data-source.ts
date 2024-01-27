9b9d1d9c79150f5f6cc19c85fc88573da1ee0e4f
Fagner Valente <fagnervalente@gmail.com>
2024-01-27 01:00:18 -0300

    Fix env variables reference + add mongo package

--- src/adapter/database/data-source.ts
+++ src/adapter/database/data-source.ts
@@ -5,11 +5,11 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: 'mongodb',
   ssl: false,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT as number | undefined,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_MONGO_HOST,
+  port: process.env.DATABASE_MONGO_PORT as number | undefined,
+  username: process.env.DATABASE_MONGO_USER,
+  password: process.env.DATABASE_MONGO_PASSWORD,
+  database: process.env.DATABASE_MONGO_NAME,
   synchronize: true,
   entities: [`${__dirname}/models/*.{ts,js}`],
   migrations: [`${__dirname}/migrations/*.{ts,js}`]
