1dde4e86d5b1a5fe3c1e10fb385cd81a5c284e41
Fagner Valente <fagnervalente@gmail.com>
2024-03-23 01:38:59 -0300

    Remove get full order method

--- src/app/useCase/CreateUseCase.ts
+++ src/app/useCase/CreateUseCase.ts
@@ -1,8 +1,6 @@
 import { OrderQueue, OrderStatus } from "@entities/OrderQueue";
 import IOrderQueueRepository from "@ports/IOrderQueueRepository";
 import AbstractUseCase from "./AbstractUseCase";
-import got from "got";
-import { OrderPaymentStatus } from "@database/models/OrderQueueModel";
 
 export default class CreateUseCase extends AbstractUseCase {
 
@@ -11,36 +9,16 @@ export default class CreateUseCase extends AbstractUseCase {
 	}
 
 	public async execute(orderQueue: OrderQueue): Promise<OrderQueue | null> {
-		const orderId = await this.getParsedOrderId(orderQueue);
+		if (!orderQueue.orderId) {
+			this.setError({ message: "Order id required!" });
+		}
 
 		if (this.hasErrors()) {
 			return null;
 		}
 
-		orderQueue.orderId = orderId;
-
-		if (orderQueue.paymentStatus === OrderPaymentStatus.RECUSADO) {
-			orderQueue.status = OrderStatus.FINALIZADO;
-		} else {
-			orderQueue.status = OrderStatus.RECEBIDO;
-		}
-		
-		const orderEndpoint = process.env.ORDER_SERVICE_ENDPOINT as string;
-		await got.put(`${orderEndpoint}/paymentStatus/${orderId}`, { json: { status: orderQueue.paymentStatus } });
+		orderQueue.status = OrderStatus.RECEBIDO;
 
 		return await this.repository.save(orderQueue);
-	}
-
-	private async getParsedOrderId(order: OrderQueue): Promise<string | undefined> {
-		if (!order.orderId) {
-			this.setError({ message: "Order id required!" });
-		} else {
-			const orderEndpoint = process.env.ORDER_SERVICE_ENDPOINT as string;
-			const response = await got.get(`${orderEndpoint}/payment/${order.orderId}`);
-			if (response.statusCode != 200) {
-				this.setError(JSON.parse(response.body))
-			}
-		}
-		return order.orderId;
 	}
 }
